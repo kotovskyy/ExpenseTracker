a48db7b1e913481925499c8a83f508975057bba6
acodered <alexkondratru@gmail.com>
2023-11-23 11:40:03 +0100

    Create database models for expensetracker

--- expensetracker/core/models.py
+++ expensetracker/core/models.py
@@ -1,3 +1,158 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+import datetime
+
+from .validators import positive_decimal_validator
+
+class Currency(models.Model):
+    code = models.CharField(
+        max_length=3,
+        unique=True
+    )
+    name = models.CharField(
+        max_length=40,
+        unique=True
+    )
+    exchange_rates = models.JSONField()
+    
+    def __str__(self) -> str:
+        return f"{self.code}    {self.name}"
+    
+
+class Settings(models.Model):
+    USD = "USD"
+    PLN = "PLN"
+    EUR = "EUR"
+    MAIN_CURRENCY_CHOICES = [
+        (USD, "United States Dollar"),
+        (PLN, "Polish Złoty"),
+        (EUR, "Euro")
+    ]
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="settings"
+    )
+    main_currency = models.ForeignKey(
+        Currency,
+        on_delete=models.PROTECT,
+        default=1
+    )
+    
+    def __str__(self) -> str:
+        return f"{self.main_currency}"
+
+
+class Category(models.Model):
+    INCOME = "I"
+    EXPENSE = "E"
+    CATEGORY_TYPE_CHOICES = [
+        (INCOME, "Income"),
+        (EXPENSE, "Expense")
+    ]
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="categories"
+    )
+    name = models.CharField(
+        max_length=50
+    )
+    category_type = models.CharField(
+        max_length=1,
+        choices=CATEGORY_TYPE_CHOICES,
+        default=EXPENSE
+    )
+    total = models.DecimalField(
+        max_digits=22,
+        decimal_places=2,
+    )
+    
+    def __str__(self) -> str:
+        return f"{self.name} - {self.total} \
+            {self.user.settings.first().main_currency.code}"
+
+
+class Account(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="accounts"
+    )
+    name = models.CharField(
+        max_length=50
+    )
+    balance = models.DecimalField(
+        max_digits=22,
+        decimal_places=2
+    )
+    currency = models.ForeignKey(
+        Currency,
+        on_delete=models.PROTECT,
+        related_name="accounts"
+    )
+    description = models.CharField(
+        max_length=100,
+        blank=True
+    )
+    
+    def __str__(self) -> str:
+        return f"{self.name}    {self.balance} {self.currency.code}"
+    
+
+class Transaction(models.Model):
+    INCOME = "I"
+    EXPENSE = "E"
+    TRANSFER = "T"
+    TRANSACTION_TYPE_CHOICES = [
+        (EXPENSE, "Expense"),
+        (INCOME, "Income"),
+        (TRANSFER, "Transfer")
+    ]
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="transactions"
+    )
+    account = models.ForeignKey(
+        Account,
+        on_delete=models.CASCADE,
+        related_name="transactions"
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="transactions"
+    )
+    transaction_type = models.CharField(
+        max_length=1,
+        choices=TRANSACTION_TYPE_CHOICES,
+        default=EXPENSE
+    )
+    amount = models.DecimalField(
+        max_digits=22,
+        decimal_places=2,
+        validators=[positive_decimal_validator]
+    )
+    currency = models.ForeignKey(
+        Currency,
+        on_delete=models.PROTECT,
+        default=1
+    )
+    date = models.DateField(
+        default=datetime.date.today
+    )
+    description = models.CharField(
+        max_length=100,
+        blank=True
+    )
+    
+    def __str__(self) -> str:
+        amount_sign = "-"
+        if self.transaction_type == self.INCOME:
+            amount_sign = "+"
+        elif self.transaction_type == self.TRANSFER:
+            amount_sign = ""
+        return f"{self.category.name} {self.account.name} \
+                {amount_sign + str(self.amount)} {self.description}"
