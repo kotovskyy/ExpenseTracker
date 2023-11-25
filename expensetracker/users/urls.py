2ad06754d128c85433125928b15f7414990c51f1
acodered <alexkondratru@gmail.com>
2023-11-25 23:33:15 +0100

    Add signup and login views and templates

---
+++ expensetracker/users/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('login/', views.userLogin, name='users_userLogin'),
+    path('signup/', views.userSignUp, name='users_userSignUp'),
+]
--- expensetracker/users/views.py
+++ expensetracker/users/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+def userLogin(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'users/loginpage.html')
+
+def userSignUp(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'users/signup.html')
+
+
