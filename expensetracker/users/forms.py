a4267e3a74bfcce45b8aa9c975998575e74a6e76
acodered <alexkondratru@gmail.com>
2023-11-29 15:35:33 +0100

    Create signup form

--- expensetracker/users/forms.py
+++ expensetracker/users/forms.py
@@ -1,7 +1,22 @@
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 from .models import User
 
+class CustomPasswordInput(forms.PasswordInput):
+    def __init__(self, attrs=None, render_value=False):
+        super().__init__(attrs={'class': 'auth-form-field',
+                                'placeholder': 'password...'})
+
 class SignupForm(UserCreationForm):
+    password1 = forms.CharField(widget=CustomPasswordInput)
+    password2 = forms.CharField(widget=CustomPasswordInput)
+    
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'auth-form-field',
+                                               'placeholder': 'username...'}),
+            'email': forms.EmailInput(attrs={'class': 'auth-form-field',
+                                             'placeholder': 'email...'}),
+        }
