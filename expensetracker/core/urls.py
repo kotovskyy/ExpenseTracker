5f070789921b6279ac0819d6f6c7e2e51d5e7e04
acodered <alexkondratru@gmail.com>
2023-11-30 00:56:05 +0100

    Add categories page

--- expensetracker/core/urls.py
+++ expensetracker/core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="core_index"),
+    path('categories/', views.categories, name='core_categories'),
 ]
--- expensetracker/core/views.py
+++ expensetracker/core/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
     return render(request, 'core/index.html', context={
         "logged_in" : request.user.is_authenticated,
+    })
+
+
+@login_required
+def categories(request):
+    user = request.user
+    categories = user.categories.filter(category_type='E').order_by('id')
+    return render(request, 'core/categories.html', context={
+        'categories': categories,
     })
 
