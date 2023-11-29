b43851c4e79c000a3917ec54c443a51835d93c11
acodered <alexkondratru@gmail.com>
2023-11-29 14:58:20 +0100

    Create login page

--- expensetracker/users/views.py
+++ expensetracker/users/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from .forms import (UserCreationForm)
 
 def userLogin(request):
     if request.method == 'POST':
         pass
-    return render(request, 'users/loginpage.html')
+    form = UserCreationForm()
+    return render(request, 'users/login.html', context={
+        'form': form,
+    })
 
 def userSignUp(request):
     if request.method == 'POST':
