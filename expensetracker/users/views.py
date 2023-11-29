d27324efccd3f1626913bfda7682a8315c118b89
acodered <alexkondratru@gmail.com>
2023-11-29 20:29:50 +0100

    Add remember me functionality

--- expensetracker/users/views.py
+++ expensetracker/users/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 from .forms import (SignupForm)
 from .models import User
+from core.models import (Settings,
+                         Category)
 
 def userLogin(request):
     if request.method == 'POST':
         email_or_username = request.POST["email_or_username"]
         password = request.POST["password"]
+        remember_me = request.POST.get('remember_me', False)
         try:
             username = User.objects.get(email=email_or_username)
         except User.DoesNotExist:
@@ -17,6 +21,9 @@ def userLogin(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            if not remember_me:
+                request.session.set_expiry(0)
+            
             return HttpResponseRedirect(reverse('core_index'))
         else:
             return render(request, "users/login.html", context={
