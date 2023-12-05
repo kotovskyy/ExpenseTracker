55eb59a3cf8a8146d0903d4bc1a2418083751fb1
acodered <alexkondratru@gmail.com>
2023-12-05 20:35:47 +0100

    Add script for changing periods

--- expensetracker/core/static/core/scripts/categories.js
+++ expensetracker/core/static/core/scripts/categories.js
@@ -1,3 +1,30 @@
+function changePeriod(direction) {
+    const monthNames = [
+        'January', 'February', 'March', 'April', 'May', 'June',
+        'July', 'August', 'September', 'October', 'November', 'December'
+    ];
+
+    const currentPeriod = document.querySelector('.period-text').innerText.trim().split(' ');
+    const currentMonth = currentPeriod[0];
+    const currentYear = currentPeriod[1];
+
+    const monthNumber = monthNames.indexOf(currentMonth) + 1;
+
+    var newMonth = (direction === 'prev') ? monthNumber-1 : monthNumber+1;
+    var newYear = parseInt(currentYear);
+    
+    if (newMonth > 12) {
+        newMonth = 1;
+        newYear = newYear + 1;
+    }
+    if (newMonth <= 0) {
+        newMonth = 12;
+        newYear = newYear - 1;
+    }
+
+    window.location.href = `/categories/?month=${newMonth}&year=${newYear}`;
+}
+
 function openCategoryPage(category_id){
     var cat_id = parseInt(category_id);
     window.location.href = `/categories/${cat_id}/`;
@@ -11,6 +38,19 @@ function editCategory(category_id){
 function editAccount(account_id) {
     let form = document.getElementById("editAccountForm_"+account_id);
     form.submit();
+}
+
+function editTransaction(transaction_id) {
+    let form = document.getElementById("editTransactionForm_"+transaction_id);
+    form.submit();
+}
+
+function deleteTransaction(transaction_id) {
+    let confirmDelete = confirm("Are you sure you want to delete this transaction?\nYou won't be able to restore removed data.");
+    if (confirmDelete) {
+        let form = document.getElementById("deleteTransactionForm_"+transaction_id);
+        form.submit();
+    }
 }
 
 function deleteAccount(account_id, n_transactions) {
